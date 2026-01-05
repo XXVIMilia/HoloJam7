9cf62991a92c2ea30f7e730e4ef2803742305383
Vinsleif <aznbuddax@gmail.com>
2026-01-05 23:11:49 +1100

    made knockback scene and script

---
+++ Assets/Script/Knockable.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Knockable : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            print("player touched me");
+
+        }
+    }
+}
