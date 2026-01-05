5cf5c1cf3780c63b33bb741db16ecdcc996c3200
Vinsleif <aznbuddax@gmail.com>
2026-01-05 23:41:52 +1100

    created knockable car prefab & finished knock script
    knockable script induces explosive force to push the gameobject with rigidbody away. It exponentially gets stronger based on car speed to its max.

--- Assets/Script/Knockable.cs
+++ Assets/Script/Knockable.cs
@@ -2,10 +2,22 @@ using UnityEngine;
 
 public class Knockable : MonoBehaviour
 {
+    [Header("Parameters")]
+    public float _radius = 5.0f;
+    public float _power = 10.0f;
+    public float _upwardsModifier = 3.0f;
+    public ForceMode _forceMode = ForceMode.Force;
+
+    [Header("References")]
+    [SerializeField] private Rigidbody _rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        if (_rb == null)
+        {
+            _rb = this.gameObject.GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
@@ -18,9 +30,11 @@ public class Knockable : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-
-            print("player touched me");
-
+            if (other.gameObject.TryGetComponent<Car>(out Car car))
+            {
+                float percent = 0.7f + (car.carSpeed / car.topSpeed);
+                _rb.AddExplosionForce(Mathf.Pow(_power, percent), transform.position, _radius, Mathf.Pow(_upwardsModifier, percent), _forceMode);
+            }
         }
     }
 }
